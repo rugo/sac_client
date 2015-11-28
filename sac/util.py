221485ca91d49ffc8e29fcd53ec24557abb68c26
Ruben Gonzalez <rugonzal@htwg-konstanz.de>
2015-11-28 19:16:39 +0100

    added missing return statement

--- sac/util.py
+++ sac/util.py
@@ -1,4 +1,5 @@
 import os
+from base64 import b64encode
 
 DEVICE_ID_FILE_NAME = "/etc/sac/device_id"
 SECRET_FILE_NAME = "/etc/sac/secret"
@@ -8,14 +9,17 @@ def is_registered():
     return os.path.exists(SECRET_FILE_NAME)
 
 def __read_return(file_name):
-    with open(DEVICE_ID_FILE_NAME) as f:
+    with open(file_name) as f:
         return f.read()
 
 def get_device_id():
-    __read_return(DEVICE_ID_FILE_NAME)
+    return __read_return(DEVICE_ID_FILE_NAME)
 
 def get_secret():
-    __read_return(SECRET_FILE_NAME)
+    return __read_return(SECRET_FILE_NAME)
 
 def create_secret():
-    return os.urandom(DEF_SECRET_LENGTH)
+    secret = b64encode(os.urandom(DEF_SECRET_LENGTH))
+    with open(SECRET_FILE_NAME, "w") as f:
+        f.write(secret)
+    return secret
