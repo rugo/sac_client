52611a1df09063ca1c3fd07119223855a0501b80
Ruben Gonzalez <rugonzal@htwg-konstanz.de>
2016-01-06 17:12:46 +0100

    Added sacpython.so to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 rm */*.pyc
-tar cf sac.tar ../sac/sac ../sac/webinterface ../sac/scripts ../sac/keys ../sac/res
+tar cf sac.tar ../sac/sac ../sac/bin/sacpython.so ../sac/webinterface ../sac/scripts ../sac/keys ../sac/res
 echo -n 'galileoOne' > /tmp/sac/device_id
 tar rf sac.tar -C /tmp sac/device_id
-#scp -6 sac.tar root@\[fe80::9a4f:eeff:fe01:cd8c%eth0\]:/tmp
-#ssh -6 root@fe80::9a4f:eeff:fe01:cd8c%eth0 "tar xf /tmp/sac.tar -C /opt/ && chown sac:sac /opt/sac -R && mkdir /opt/sac/bin && ln -s /usr/local/bin/python /opt/sac/bin/python"
-scp sac.tar root@192.168.43.236:/tmp
-ssh root@192.168.43.236 "tar xf /tmp/sac.tar -C /opt/ && chown sac:sac /opt/sac -R && mkdir /opt/sac/bin && ln -s /usr/local/bin/python /opt/sac/bin/python"
+scp -6 sac.tar root@\[fe80::9a4f:eeff:fe01:cd8c%eth0\]:/tmp
+ssh -6 root@fe80::9a4f:eeff:fe01:cd8c%eth0 "tar xf /tmp/sac.tar -C /opt/ && chown sac:sac /opt/sac -R && ln -s /usr/local/bin/python /opt/sac/bin/python && ln -s /opt/sac/bin/sacpython.so /usr/local/lib/python2.7/lib-dynload/sacpython.so"
